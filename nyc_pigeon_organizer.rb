c1973117f213c294e021525180b3e119f33ce259
Kelly Powers <kpowers.nj@gmail.com>
2020-01-10 18:49:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -25,13 +25,14 @@ def nyc_pigeon_organizer(data)
   new_hash.each do |key, value|
     puts "new hash key is #{key} value is #{value}"  
     value.each do |keya, valuea|
-   data.each do |key1, value1|
-     puts "data key1 is #{key1} value1 is #{value1}"
-     if key1 == keya 
-      value1.each do |key2, value2|
-        puts "value1 key2 is #{key2} and value2 is #{value2}"
-          if key == value2
-            valuea.push(key2)
+      puts "value keya is #{keya} value valuea is #{valuea}"
+      data.each do |key1, value1|
+        puts "data key1 is #{key1} value1 is #{value1}"
+        if key1 == keya 
+          value1.each do |key2, value2|
+            puts "value1 key2 is #{key2} and value2 is #{value2}"
+              if key == value2
+                valuea.push(key2)
           end
         end
       end
