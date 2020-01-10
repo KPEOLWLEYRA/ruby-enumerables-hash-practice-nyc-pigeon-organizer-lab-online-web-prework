f4e0dc8901cefca600219d6b3514ee12347f6568
Kelly Powers <kpowers.nj@gmail.com>
2020-01-10 17:29:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,86 @@
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  array = [] 
+  pigeon_male = data[:gender][:male]
+  pigeon_female = data[:gender][:female]
+  i = 0 
+  while i < pigeon_male.length do 
+    array.push(pigeon_male[i])
+    i += 1 
+  end
+  i = 0 
+  while i < pigeon_female.length do 
+    array.push(pigeon_female[i])
+    i +=1 
+  end
+  array
+  # array.each {|i| new_hash[i] = nil
+  #^create keys 
+  # #{
+  #   :color => [],
+  #   :gender => [],
+  #   :lives => []
+  # }
+   
+  }
+  puts new_hash 
+  new_hash
+  new_hash.each do |i|
+    data.each_value do |value1|
+        data[value1].each_value do |value2|
+        data[value2].each_value do |m|
+          if data[m] == new_hash[i]
+            new_hash[value2] << data[m]
+          end
+        end
+      end
+    end
+    puts new_hash
+  end
+    # data[:color].each do |color|
+    #   data[:color]
+    #   if color_attr == new_hash[i]
+    #     new_hash[i][:color] << color_attr
+    #   end 
+    # end
+    # data[:gender].each do |mof, mof_attr|
+    #   if mof_attr == new_hash[i]
+    #     new_hash[i][:gender] << mof_attr
+    #   end
+    # end
+    # data[:lives].each do |city|
+    #   data[:lives][city].each do |j|
+    #     if j == new_hash[i]
+    #       new_hash[i][:lives]<< j
+
+
+  new_hash
 end
+
+
+
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
+#puts pigeon_data[:gender][:male][0] 
+#puts pigeon_data[:gender][:female][0]
+
+nyc_pigeon_organizer(pigeon_data)
